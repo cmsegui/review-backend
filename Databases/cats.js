0d0c4b1c386d9ec927d19d58561b1011d02b87b1
cmsegui@yahoo.com <cydmarie.dev@gmail.com>
2017-11-17 04:57:53 +0000

    reviewed mongoose

--- Databases/cats.js
+++ Databases/cats.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/cat_app");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/cat_app", {useMongoClient: true});
 
 var catSchema = new mongoose.Schema({
     name: String,
@@ -8,3 +9,18 @@ var catSchema = new mongoose.Schema({
 });
 
 var Cat = mongoose.model("Cat", catSchema);
+
+var george = new Cat({
+    name: "Tabby",
+    age: 5, 
+    temperament: "Evil"
+});
+
+george.save(function(err, cat) {
+    if(err) {
+        console.log("Something went wrong!");
+    } else {
+        console.log("A cat was saved to the database!");
+        console.log(cat);
+    }
+});
